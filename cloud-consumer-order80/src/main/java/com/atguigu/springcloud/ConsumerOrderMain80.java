7cee0c11031335d4c48ea43f36fe8021b6b2ccb7
OceanWht <328295484@qq.com>
2021-03-08 14:41:05 +0800

    服务注册中心

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrderMain80.java
@@ -1,16 +1,13 @@
 package com.atguigu.springcloud;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@MapperScan("com.atguigu.springcloud.mapper")
 @EnableDiscoveryClient
-public class PaymentMain8001 {
-
+public class ConsumerOrderMain80 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(ConsumerOrderMain80.class,args);
     }
 }
