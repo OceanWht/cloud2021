315a568250969eeb8f6a98f898f93e3e3d091932
OceanWht <328295484@qq.com>
2021-03-09 11:31:29 +0800

    服务注册中心 服务提供者8001 集群 新建一个8002

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entites.CommonResult;
 import com.atguigu.springcloud.entites.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,15 +16,18 @@ public class PaymentController {
     @Resource
     PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String port;
+
     @PostMapping(value = "/payment/create")
     public CommonResult<Payment> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("插入结果...."+result);
         if (result == 0){
-            return new CommonResult(404,"创建失败",null);
+            return new CommonResult(404,"创建失败,serverPort:"+port,null);
         }
 
-        return new CommonResult(200,"创建成功",result);
+        return new CommonResult(200,"创建成功,serverPort:"+port,result);
     }
 
 
@@ -32,9 +36,9 @@ public class PaymentController {
         Payment payment = paymentService.getById(id);
         log.info("查询结果...."+payment);
         if (payment == null){
-            return new CommonResult(404,"查询失败",null);
+            return new CommonResult(404,"查询失败，查询ID："+id+"serverPort:"+port,null);
         }
 
-        return new CommonResult(200,"查询成功，查询ID："+id,payment);
+        return new CommonResult(200,"查询成功，查询ID："+id+",serverPort:"+port,payment);
     }
 }
